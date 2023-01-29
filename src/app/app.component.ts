e726dffce80d3becb429e41385db7dc4ae9a80ad
Unpaid Worker <74233190+teamweb095@users.noreply.github.com>
2023-01-29 14:25:45 +0530

    Added product component and updated style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpCallService } from './http-call.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Product-Listing';
+  productList = [];
+  constructor(private httpCallService: HttpCallService){
+    this.productList = [];
+  }
+  
+  ngOnInit() {
+    this.httpCallService.getProducts().subscribe((productslistData: any) =>{
+      console.log(productslistData);
+      this.productList = productslistData.products;
+
+    });
+  }
 }
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+@Input() productData: any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
